33572f90cecbc37dd51f2c8fdfbef37a06c6261a
Thales Ribeiro <thalessr@gmail.com>
2023-03-13 08:45:29 -0300

    Fix railtie

--- lib/mobile_id/railtie.rb
+++ lib/mobile_id/railtie.rb
@@ -1,13 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails'
 
 module MobileId
-  class Railtie < ::Rails::Railtie #:nodoc:
+  class Railtie < ::Rails::Railtie # :nodoc:
+
     initializer 'mobile_id' do |app|
-      DeviseI18n::Railtie.instance_eval do
+      MobileId::Railtie.instance_eval do
         (app.config.i18n.available_locales & MobileId::LOCALES).each do |loc|
           I18n.load_path << File.expand_path("locales/#{loc}.yml", __dir__)
         end
       end
     end
+
   end
 end
